2cf8f0ee4461b5561f71a4aacf57878bf9802bff
YggdrasilJL <yggdrasiljl@gmail.com>
2023-09-02 21:39:55 -0300

    make thoughts model

---
+++ models/Thoughts.js
@@ -0,0 +1,55 @@
+const dayjs = require('dayjs');
+
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    max: [280, 'Cannot exceed 280 characters!'],
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtDate) => dayjs(createdAtDate).format('YYYY-MM-DD'),
+  },
+});
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max: [280, 'Cannot exceed 280 characters!'],
+      min: [1, 'Cannot be blank!'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtDate) => dayjs(createdAtDate).format('YYYY-MM-DD'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
